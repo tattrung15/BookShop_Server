69baced8fbcb32f96b3e5bfe1363d543eec60c9b
Tất Trung <buitrungt@gmail.com>
2021-02-13 15:38:38 +0700

    cross origin

--- src/main/java/com/bookshop/controllers/AuthController.java
+++ src/main/java/com/bookshop/controllers/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
