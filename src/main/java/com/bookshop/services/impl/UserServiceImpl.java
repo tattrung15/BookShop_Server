9f84827c96455d1a213da1fd06f0209b600baee8
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2021-11-23 11:23:14 +0700

    remove unnecessary import

--- src/main/java/com/bookshop/services/impl/UserServiceImpl.java
+++ src/main/java/com/bookshop/services/impl/UserServiceImpl.java
@@ -13,8 +13,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UserServiceImpl extends BasePagination<User, UserRepository> implements UserService {
 
