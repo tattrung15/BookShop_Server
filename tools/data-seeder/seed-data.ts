d74bdfeec9a96aeffee3e2ea7c9efc859a1bc58f
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2022-03-21 10:25:02 +0700

    add alter id seq for seed

--- tools/data-seeder/seed-data.ts
+++ tools/data-seeder/seed-data.ts
@@ -7,13 +7,22 @@ const prisma = new PrismaClient();
 
 async function seedData() {
   await prisma.banners.createMany({ data: banners });
+  await prisma.$executeRawUnsafe(
+    `ALTER SEQUENCE banners_id_seq RESTART WITH ${banners.length + 1}`
+  );
   await prisma.categories.createMany({ data: categories });
+  await prisma.$executeRawUnsafe(
+    `ALTER SEQUENCE categories_id_seq RESTART WITH ${categories.length + 1}`
+  );
   await prisma.products.createMany({ data: products });
+  await prisma.$executeRawUnsafe(
+    `ALTER SEQUENCE products_id_seq RESTART WITH ${products.length + 1}`
+  );
 }
 
 try {
   seedData();
-  console.log("Seed data successful");
+  console.log("Seed data successfully");
 } catch (e) {
   console.log("Seed data failed");
   console.log(e);
