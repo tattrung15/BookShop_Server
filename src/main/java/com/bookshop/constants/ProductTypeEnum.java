c1a571aefd39b420b40c1fbd7bb95bbb72c618c3
Tất Trung <buitrungt@gmail.com>
2022-03-23 21:30:34 +0700

    fix: fixed get list products with product type

--- src/main/java/com/bookshop/constants/ProductTypeEnum.java
+++ src/main/java/com/bookshop/constants/ProductTypeEnum.java
@@ -3,4 +3,5 @@ package com.bookshop.constants;
 public class ProductTypeEnum {
     public static final String HAVE_IMAGE = "have-image";
     public static final String NO_IMAGE = "no-image";
+    public static final String NO_IMAGE_ALL = "no-image-all";
 }
