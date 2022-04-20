96c56a1c6aed8d37ae0855b46a39e8e6847ba4ae
Tất Trung <buitrungt@gmail.com>
2022-04-20 16:56:23 +0700

    remove import redundant

--- src/main/java/com/bookshop/configs/WebSecurityConfig.java
+++ src/main/java/com/bookshop/configs/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.bookshop.configs;
 
 import com.bookshop.exceptions.CustomEntryPoint;
-import com.bookshop.exceptions.ForbiddenException;
 import com.bookshop.filters.JwtRequestFilter;
 import com.bookshop.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +14,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
